b1d105bc9023008e4982650f90ee927e57c9d05b
Sultepronit <sultepronit@gmail.com>
2022-08-07 22:18:56 +0300

    started lesson about passing a value

---
+++ Passing by value/src/App.java
@@ -0,0 +1,14 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		App app = new App();
+		
+		app.show(7);
+	}
+	
+	public void show(int value) {
+		System.out.println("value is :" + value);
+	}
+
+}
