5653c3d8bb2e8c12789d15935856883abd93646b
Sultepronit <sultepronit@gmail.com>
2022-08-16 18:50:54 +0300

    practiced a little

--- Experiment/src/Exp6.java
+++ Experiment/src/Exp6.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
 
 public class Exp6 {
 
 	public static void main(String[] args) {
 
-		nextSmaller(20719);
+		//nextSmaller(20719);
 		//nextSmaller(21);
+		//nextSmaller(907);
+		//nextSmaller(12);
+		nextSmaller(1023);
 
 	}
 	
@@ -13,23 +17,48 @@ public class Exp6 {
 		String s = "" + n;
 		System.out.println(s);
 		
-		int division = 0;
-		String toChange = "";
+		int division = -1;
+		char toTheRight = ' ';
 		for(int i = s.length() - 2; i >= 0; i--) {
 			if(s.charAt(i) > s.charAt(i + 1)) {
-				toChange += s.charAt(i);
-				System.out.println("!" + i+ ":" + toChange);
+				toTheRight = s.charAt(i);
+				System.out.println("!" + i+ ":" + toTheRight);
 				division = i;
 				break;
 			}
-			System.out.println(s.charAt(i));
-		}
-		
+		}	
+		if(division < 0) return -1;
 		String s1 = s.substring(0, division);
 		String s2 = s.substring(division);
 		System.out.println(s1);
 		System.out.println(s2);
 		
-		return n;
+		char[] toSort = s2.toCharArray();
+		//System.out.println(toSort);
+		Arrays.sort(toSort);
+		System.out.println(toSort);
+		
+		char toTheLeft = ' ';
+		for(int i = toSort.length - 1; i >= 0; i--) {
+			if(toSort[i] < toTheRight) {
+				toTheLeft = toSort[i];
+				toSort[i] = '_';
+				break;
+			}
+		}
+		//System.out.println(toTheLeft);
+		System.out.println(toSort);
+		if(toTheLeft == '0') return -1;
+		
+		StringBuilder result = new StringBuilder(s1);
+		result.append(toTheLeft);
+		
+		for(int i = toSort.length - 1; i >= 0; i--) {
+			if(toSort[i] == '_') continue;
+			result.append(toSort[i]);
+		}
+		System.out.println(result);
+		
+		return Long.parseLong(result.toString());
 	}
 }
